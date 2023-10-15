bb2d30c718e6bd5185c2a85cdfb96867274f190b
SarveshGade <32420413+SarveshGade@users.noreply.github.com>
2023-10-15 05:27:30 -0400

    new css

--- Hack-GT-backend/server.js
+++ Hack-GT-backend/server.js
@@ -10,10 +10,10 @@ app.use(bodyParser.json());
 class RoadMapper {
     async processChat(skillTopic, numDays) {  // Accepting arguments here
         const openai = new OpenAI({
-            apiKey: API_KEY
+            apiKey: API
         });
 
-        var prompt = `Give me a plan with resources to learn ${skillTopic} in ${numDays} days in a table format. The table should have four columns. The first column should include the day number (say including the word Day in each row), the second column should include the topic, the third column should include multiple resources with specific links and titles, and the fourth column should include multiple tasks. The second column should only include the topic to be learned. The third column should only include multiple recommended resources to learn that topic. The fourth column should only include the multiple tasks that should be taken to learn the corresponding topic for each day. Give an equal amount of tasks and resources but make sure to give multiple. Please surround strings with double quotes. Please make sure to include and use real links. Set the temperature setting to 0.1.`;
+        var prompt = `Give me a plan with resources to learn ${skillTopic} in ${numDays} days in a table format. The table should have four columns. The first column should include the day number (say including the word Day in each row), the second column should include the topic, the third column should include multiple resources with specific links and titles, and the fourth column should include multiple tasks. The second column should only include the topic to be learned. The third column should only include multiple recommended resources to learn that topic. The fourth column should only include the multiple tasks that should be taken to learn the corresponding topic for each day. Give an equal amount of tasks and resources but make sure to give multiple. Please surround strings with double quotes. Please make sure to include and use real links. Combine the multiple suggestions into one cell of the table. Set the temperature setting to 0.1.`;
 
         const chatCompletion = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
@@ -21,6 +21,7 @@ class RoadMapper {
         });
 
         const output = chatCompletion.choices[0].message.content;
+        console.log(output);
         const separatedOutput = output.split("Day ");
 
         for (let i = 0; i < separatedOutput.length; i++) {
@@ -36,7 +37,7 @@ class RoadMapper {
 
         separatedOutput.splice(0, 1);
         separatedOutput.shift();
-
+        console.log(separatedOutput);
         return separatedOutput;
     }
 }
